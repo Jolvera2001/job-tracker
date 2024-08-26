4a1a1c88c2f643be846394fafd3b9739f1f3b6d9
Jolvera2001 <jolvera6@stedwards.edu>
2024-08-26 11:34:17 -0500

    putting down though process

--- internal/features/auth/auth_services.go
+++ internal/features/auth/auth_services.go
@@ -1,7 +1,19 @@
 package auth
 
+import (
+	"firebase.google.com/go/auth"
+)
+
 func RegisterService(registerDto UserRegisterDto) (string, error) {
-	
+	params := (&auth.UserToCreate{}).
+		Email(registerDto.Email).
+		Password(registerDto.Password)
+
+	// create user record
+	// create custom token using user record uid
+	// create user within mongoDB and store if and only if the previous two are successful
+
+	// return token, user object ID, and nil error
 }
 
 func LoginService(loginDto UserLoginDto) (string, error) {
