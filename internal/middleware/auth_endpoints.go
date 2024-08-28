be05210ba313aa403ef1324b074300503973ddbb
Jolvera2001 <jolvera6@stedwards.edu>
2024-08-28 10:29:21 -0500

    added auth middleware

---
+++ internal/middleware/auth_endpoints.go
@@ -0,0 +1,40 @@
+package middleware
+
+import (
+	"context"
+	"job-tracker/internal/firebase"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is required"})
+			c.Abort()
+			return
+		}
+
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+		if tokenString == authHeader {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid Authorization header format"})
+			c.Abort()
+			return
+		}
+
+		token, err := firebase.Auth_Client.VerifyIDToken(context.Background(), tokenString)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid or expired token"})
+			c.Abort()
+			return
+		}
+
+		// sets key value pair for use within specific REQUEST's context
+		// can use c.Get("user") to get token for whatever now
+		c.Set("user", token)
+		c.Next()
+	}
+}
