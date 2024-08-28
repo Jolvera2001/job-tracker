056db13e822ad52656c3a1361b6a3752d1b9ec3d
Jolvera2001 <jolvera6@stedwards.edu>
2024-08-28 10:56:12 -0500

    refactored authentication logic to use UID from firebase

--- internal/features/auth/user_model.go
+++ internal/features/auth/user_model.go
@@ -4,6 +4,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type UserModel struct {
 	ID          primitive.ObjectID `json:"_id" bson:"_id"`
+	UserID      string             `json:"user_id" bson:"user_id"`
 	Username    string             `json:"username" bson:"username"`
 	Email       string             `json:"email" bson:"email"`
 	DateCreated primitive.DateTime `json:"date_created" bson:"date_created"`
