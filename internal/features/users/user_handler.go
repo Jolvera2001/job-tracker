0f28e8de0f5becc53b25a74daa835d60cc8751c5
Jolvera2001 <jolvera6@stedwards.edu>
2024-08-28 16:41:39 -0500

    implemented handlers

--- internal/features/users/user_handler.go
+++ internal/features/users/user_handler.go
@@ -2,6 +2,8 @@ package users
 
 import (
 	"job-tracker/internal/middleware"
+	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,13 +18,41 @@ func GroupUserHandlers(r *gin.Engine) {
 }
 
 func GetUserHandler(c *gin.Context) {
+	response, err := GetUserService(c)
+	if err != nil {
+		log.Println(err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
+	c.JSON(http.StatusOK, gin.H{"user": response})
 }
 
 func UpdateUserHandler(c *gin.Context) {
-	
+	var update UserUpdateDto
+	if err := c.BindJSON(&update); err != nil {
+		log.Println(err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	response, err := UpdateUserService(c, update)
+	if err != nil {
+		log.Println(err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"user": response})
 }
 
 func DeleteUserHandler(c *gin.Context) {
-	
+	err := DeleteUserService(c)
+	if err != nil {
+		log.Println(err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"status": "success"})
 }
