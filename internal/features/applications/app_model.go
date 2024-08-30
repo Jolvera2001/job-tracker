bf65719a9aa471101b4683beca4b67cacd9a92a0
Jolvera2001 <jolvera6@stedwards.edu>
2024-08-30 17:13:29 -0500

    defined application model and implemented getappallservice

--- internal/features/applications/app_model.go
+++ internal/features/applications/app_model.go
@@ -1,5 +1,13 @@
 package applications
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type AppModel struct {
-	
+	ID          primitive.ObjectID `json:"_id" bson:"_id"`
+	BatchId     primitive.ObjectID `json:"batch_id" bson:"batch_id"`
+	Name        string             `json:"name" bson:"name"`
+	Company     string             `json:"company" bson:"company"`
+	Description string             `json:"description" bson:"description"`
+	Status      string             `json:"status" bson:"status"`
+	RoundCount  int                `json:"round_count" bson:"round_count"`
 }
--- internal/features/applications/application_services.go
+++ internal/features/applications/application_services.go
@@ -1,19 +1,42 @@
 package applications
 
 import (
-	"fmt"
+	"context"
+	"job-tracker/internal/database"
 
-	"firebase.google.com/go/auth"
-	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
+const(
+	colName = "Applications"
+)
 func GetAppService(appId primitive.ObjectID) (AppModel, error) {
 
 }
 
 func GetAppAllService(batchId primitive.ObjectID) ([]AppModel, error) {
+	filter := bson.M{"batch_id": batchId}
 
+	var appList []AppModel
+	cur, err := database.GetCollection(colName).Find(context.Background(), filter)
+	if err != nil {
+		return []AppModel{}, err
+	}
+
+	defer func() {
+		if closeError := cur.Close(context.Background()); closeError != nil {
+			err = closeError
+		}
+	}()
+
+	err = parseCursor(&appList, cur)
+	if err != nil {
+		return []AppModel{}, err
+	}
+
+	return appList, nil
 }
 
 func CreateAppService(appDto AppDto) (AppModel, error) {
@@ -26,4 +49,21 @@ func UpdateAppService(appDto AppDto) (AppModel, error) {
 
 func DeleteAppService(appId primitive.ObjectID) {
 
+}
+
+func parseCursor(appList *[]AppModel, cursor *mongo.Cursor) error {
+	for cursor.Next(context.Background()) {
+		var app AppModel
+		if err := cursor.Decode(&app); err != nil {
+			return err
+		}
+
+		*appList = append(*appList, app)
+	}
+
+	if err := cursor.Err(); err != nil {
+		return err
+	}
+
+	return nil
 }
