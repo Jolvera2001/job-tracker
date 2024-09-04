e9ff23960f093edce559753b13fb42d93073d381
Jolvera2001 <jolvera6@stedwards.edu>
2024-09-04 18:17:45 -0500

    checking environment variables and added logging

--- internal/database/mongodb.go
+++ internal/database/mongodb.go
@@ -17,22 +17,28 @@ var MongoClient *mongo.Client
 
 func ConnectToMongoDB() error {
 	uri := os.Getenv("MDB_STR")
+	if uri == "" {
+		log.Fatalln("MDB_STR NOT SET!")
+	}
 
 	clientOptions := options.Client().ApplyURI(uri)
 	clientOptions.SetTLSConfig(nil)
 
+	// attempting connection
+	log.Println("Attempting to connect to mongodb...")
 	client, err := mongo.Connect(context.Background(), clientOptions)
 	if err != nil {
 		return err
 	}
 
+	log.Println("Pinging Mongodb...")
 	err = client.Ping(context.TODO(), nil)
 	if err != nil {
 		return err
 	}
 
+	log.Println("MongoDB Connection successful!")
 	MongoClient = client
-
 	return nil
 }
 
